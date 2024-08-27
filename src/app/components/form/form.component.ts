f4a279e2ffaa43b9564585336819d7da3d449dac
vicbolt <vicbolt.madrid@gmail.com>
2024-08-27 19:06:54 +0200

    last fixes

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
-export class FormComponent {
 
+export class FormComponent {
+  contactoForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactoForm = this.fb.group({
+      nombre: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telefono: [''],
+      motivo: ['', Validators.required],
+      mensaje: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactoForm.valid) {
+      // Aquí se enviaría el formulario a través de un servicio, por ejemplo.
+      console.log(this.contactoForm.value);
+      // Lógica para enviar el correo electrónico
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
